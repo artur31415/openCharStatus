2ace25a656477ed350775dcedcb92d0b5ab9e391
Artur Ferreira Moreira <artur31415926@gmail.com>
2022-05-24 11:57:44 -0300

    code

--- Scripts/Equipment.gd
+++ Scripts/Equipment.gd
@@ -1,4 +1,9 @@
 class_name Equipment extends BaseItem
 
+enum Type {
+    WEAPON,
+    ARMOR
+}
+
 func _init():
     pass
--- Scripts/Monster.gd
+++ Scripts/Monster.gd
@@ -5,6 +5,21 @@ var Name
 
 var baseStatus
 
+var gear = {
+    "head": null,
+
+    "left_hand": null,
+    "right_hand": null,
+
+    "armor": null,
+    "arms": null,
+    "legs": null,
+
+    "accessory": null
+}
+
+var inventory = []
+
 func _init(_Key, _Name, _baseStatus):
     Key = _Key
     Name = _Name
